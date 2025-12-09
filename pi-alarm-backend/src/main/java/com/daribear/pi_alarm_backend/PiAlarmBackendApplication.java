5b37728e8e929e5ce040d6739e6c32cab64b9b9d
NotDari <dariushtomlinson@gmail.com>
2025-12-09 15:52:24 +0000

    Created a jupyter notebook, and created Data configuration steps that allows for data preparation for YOLO training.
    Setup up a blank SpringBoot Project for the backend of the server.

---
+++ pi-alarm-backend/src/main/java/com/daribear/pi_alarm_backend/PiAlarmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.daribear.pi_alarm_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiAlarmBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiAlarmBackendApplication.class, args);
+	}
+
+}
---
+++ pi-alarm-backend/src/test/java/com/daribear/pi_alarm_backend/PiAlarmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daribear.pi_alarm_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiAlarmBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

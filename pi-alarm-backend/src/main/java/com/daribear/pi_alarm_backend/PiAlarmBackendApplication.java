a4e8614a41d4f002a58168703bedfd449bf5801b
NotDari <dariushtomlinson@gmail.com>
2025-12-10 12:35:37 +0000

    Created database with .env details Modified backend to connect to database

--- pi-alarm-backend/src/main/java/com/daribear/pi_alarm_backend/PiAlarmBackendApplication.java
+++ pi-alarm-backend/src/main/java/com/daribear/pi_alarm_backend/PiAlarmBackendApplication.java
@@ -1,13 +1,21 @@
 package com.daribear.pi_alarm_backend;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
 
 @SpringBootApplication
-public class PiAlarmBackendApplication {
+public class PiAlarmBackendApplication implements CommandLineRunner{
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(PiAlarmBackendApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
 	}
 
 }
